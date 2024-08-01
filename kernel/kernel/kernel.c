104225bcabcca2ae0718fdd571d52e1bdabb93c1
Ethan Graham <ethan.w.s.graham@gmail.com>
2024-08-01 22:31:26 +0200

    Set up multiboot for memory detection.

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
+#include <kernel/multiboot.h>
+#include <stdlib.h>
 
 // this is the main entry-point for the kernel woohoo!
-void kernel_main(void) {
+void kmain(multiboot_info_t* mbd, unsigned int magic) {
 	terminal_initialize();
-	printf("printing integer: %d\n", 2311);
-	printf("printing character: %c\n", 'u');
-	printf("printing string: %s\n", "Hello, world!");
-	printf("printing hex: %x\n", 0xFFFF);
-	int tmp = printf("printing hex: %x\n", 0x2A3B);
-	printf("prev printf returned %d", tmp);
+
+    printf("expected magic: %x\n", MULTIBOOT_BOOTLOADER_MAGIC);
+    printf("actual magic: %x\n", magic);
+
+    if (magic != MULTIBOOT_BOOTLOADER_MAGIC) {
+        // kernel panic
+        abort();
+    }
 }
