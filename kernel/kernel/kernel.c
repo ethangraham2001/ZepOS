cb449045961f5fc5422add0bd8f84d4cfe44748c
Ethan Graham <ethan.w.s.graham@gmail.com>
2024-08-01 13:57:32 +0200

    Added basic printf formatting for integers, string, characters, and hex-formatted unsigned

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -5,5 +5,10 @@
 // this is the main entry-point for the kernel woohoo!
 void kernel_main(void) {
 	terminal_initialize();
-	printf("Hello, kernel World! This is ZepOS!\nNewline uwu");
+	printf("printing integer: %d\n", 2311);
+	printf("printing character: %c\n", 'u');
+	printf("printing string: %s\n", "Hello, world!");
+	printf("printing hex: %x\n", 0xFFFF);
+	int tmp = printf("printing hex: %x\n", 0x2A3B);
+	printf("prev printf returned %d", tmp);
 }
