f7bf9635eb2946e3d8ecbe58db7f6c349b9e8b96
Ethan Graham <ethan.w.s.graham@gmail.com>
2024-08-03 18:39:15 +0200

    Added kernel `panic(const char* reason)` to libc/stdlib

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -10,6 +10,9 @@ extern "C" {
 __attribute__((__noreturn__))
 void abort(void);
 
+__attribute__((__noreturn__))
+void panic(const char* reason);
+
 #ifdef __cplusplus
 }
 #endif
---
+++ libc/stdlib/panic.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+__attribute__((__noreturn__))
+void panic(const char* reason) {
+#if defined(__is_libk)
+	// TODO: Add proper kernel panic.
+	printf("kernel panic: %s\n", reason);
+#else
+	// TODO: Abnormally terminate the process as if by SIGABRT.
+	printf("kernel panic: %s\n", reason);
+#endif
+	while (1) { }
+	__builtin_unreachable();
+}
